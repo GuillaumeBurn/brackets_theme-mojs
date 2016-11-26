7652ac30813a29afa47f71b122350004272b1b0a
GuillaumeBurn <burnstudioproduction@gmail.com>
2016-11-26 00:20:57 -0500

    custom-theme-v.0.0.1

---
+++ main.js
@@ -0,0 +1,7 @@
+define(function (require, exports, module) {
+    "use strict";
+
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+    ExtensionUtils.addEmbeddedStyleSheet(".consola { font-family: 'yolo'; font-size:14px; }");
+});
